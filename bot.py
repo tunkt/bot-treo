359660f55b3a076f0d5f0e8a35064a3870ea66c6
tunkt <tunkiet7744@gmail.com>
2026-08-14 03:14:26 +0700

    Add Flask server to Discord bot

--- bot.py
+++ bot.py
@@ -1,3 +1,13 @@
+from flask import Flask
+from threading import Thread
+app = Flask('')
+@app.route('/')
+def home():
+    return "Bot is alive!"
+def run():
+    app.run(host='0.0.0.0', port=8080)
+t = Thread(target=run)
+t.start()
 import os
 import discord
 from discord.ext import commands
